b607bb6770b1166b7fa9210e27d0512e07dad84b
vijen012 <vijender08malav@gmail.com>
2017-02-28 14:54:39 +0530

    Committing Ant Project

---
+++ ApacheAnt/src/com/test/TestClient.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class TestClient
+{
+	@Test
+	public void testLaunchURL()
+	{
+		System.out.println("TestClient - Lauch URL");
+	}
+	
+	@Test
+	public void testNavigateToURL()
+	{
+		System.out.println("TestClient - Navigated to google.com");
+	}
+}
---
+++ ApacheAnt/src/com/test/TestManager.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class TestManager 
+{
+	@Test
+	public void testLaunchURL()
+	{
+		System.out.println("TestManager - Lauch URL");
+	}
+	
+	@Test
+	public void testNavigateToURL()
+	{
+		System.out.println("TestManager - Navigated to google.com");
+	}
+}
